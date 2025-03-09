f979842d9b152dad4f6efba48d4065c4517ded9b
kwak yun jik <rhkrdbswlr@gmail.com>
2025-03-10 00:20:56 +0900

    demoTravel + kakaomap and location marker

--- src/main/java/org/example/demo_travel/map/RestMapController.java
+++ src/main/java/org/example/demo_travel/map/RestMapController.java
@@ -1,17 +1,22 @@
-package org.example.demo_travel.travel;
+package org.example.demo_travel.map;
 
 import lombok.extern.slf4j.Slf4j;
+
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+
 @Slf4j
 @Controller
-@RequestMapping("/travel")
+@RequestMapping("/map")
 public class RestMapController {
     @GetMapping("/kakaoMap_do")
     public String kakaoMap_do() {
         log.info("kakaoMap_do()...");
         return "map/kakao_map";
     }
+
 }
